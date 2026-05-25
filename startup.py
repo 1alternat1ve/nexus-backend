4344ace0dcca40a5fa5ff58dc001879d0e00ad9a
Nexus <nexus@nexus.app>
2026-05-26 00:27:25 +0200

    Use PORT env var for uvicorn

--- startup.py
+++ startup.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 
 async def run_api():
     import uvicorn
-    config = uvicorn.Config("main:app", host="0.0.0.0", port=8000, log_level="info")
+    port = int(os.environ.get("PORT", 8000))
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=port, log_level="info")
     server = uvicorn.Server(config)
     await server.serve()
 
